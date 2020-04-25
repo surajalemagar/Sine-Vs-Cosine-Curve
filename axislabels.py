b2ba331ea0d445306a29647f2d3c95c44a0ed237
suraj <surajido111@gmail.com>
2020-04-25 11:02:23 +0545

    label and legend rule in comment

--- axislabels.py
+++ axislabels.py
@@ -3,11 +3,12 @@ import numpy as np
 x=np.arange(0,4*np.pi-1,0.1)# start,stop,step
 y=np.sin(x)
 z=np.cos(x)
+
 #plt.plot(x,y)
 #plt.plot(x,z)
 plt.plot(x,y,x,z)
-plt.xlabel("values of x is from 0 to 4pi")
+plt.xlabel("values of x is from 0 to 4pi")  #string must be enclosed with quotes '  '
 plt.ylabel('sin(x) and cos(x)')
 plt.title('Plot of Sin(x) and Cos(x)')
-plt.legend(['sin(x)','cos(x)'])
+plt.legend(['sin(x)','cos(x)']) # legend entries as seperate strings in a list
 plt.show()
