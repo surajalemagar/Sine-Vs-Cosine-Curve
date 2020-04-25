c28e790fae475773b6d1f1af6214d0922f4cf800
krisamagar <64309065+krisamagar@users.noreply.github.com>
2020-04-25 16:35:39 +0545

    trying

--- axislabels.py
+++ axislabels.py
@@ -12,5 +12,5 @@ plt.plot(x,y,x,z)
 plt.xlabel("values of x is from 0 to 4pi")  #string must be enclosed with quotes '  '
 plt.ylabel('sin(x) and cos(x)')
 plt.title('Plot of Sin(x) and Cos(x)')
-plt.legend(['sin(x)','cos(x)']) # legend entries as seperate strings in a list
+plt.legend(['sin(x)','cos(x)']) # legend entries as seperate strings in a list sorry master file
 plt.show()
